b80c81390f286fa7289e3708c5ae2654b5243279
Kaifeng Zhang <kf.zhang@gmail.com>
2026-01-17 16:56:36 +0800

    更新博客标题

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '技术叙事与内容工程';
+export const SITE_DESCRIPTION = '分享技术与内容';
