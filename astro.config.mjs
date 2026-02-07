2c8db5300f59379b8cb931d39e550bc2be3c7849
Kaifeng Zhang <kf.zhang@gmail.com>
2026-02-07 16:57:43 +0800

    Force Vercel redeploy - update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// Force redeploy: 2026-02-07-
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
